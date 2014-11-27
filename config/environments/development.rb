c49be2f4a28d1e969040f5c16d613403a6182fa4
Carlos Interone <carlos@mostlyclicks.com>
2014-11-27 07:47:38 -0600

    change domain to timesportusa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,25 @@ Time06::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
+  # if config.respond_to?(:action_mailer)
+  #   config.action_mailer.raise_delivery_errors = false
+  # end
+
+    # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
-    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = true
   end
+
+  config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'timesportusa.com',
+  :enable_starttls_auto => true
+}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
